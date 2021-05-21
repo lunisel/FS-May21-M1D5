3d90c0965fcb93c7911ebb97abb3f2fa6e105501
lunisel <selene942008@hotmail.com>
2021-05-21 15:24:46 +0200

    Exercise 3 solved

--- index.js
+++ index.js
@@ -41,6 +41,21 @@ Write a function "crazyDiff" that computes the absolute difference between a giv
 It should return triple their absolute difference if the given number is greater than 19.
 */
 
+const crazyDiff = function(firstGivenNumber, secondGivenNumber = 19){
+    let firstDifferece = secondGivenNumber - firstGivenNumber
+    if (firstGivenNumber >= 19){
+        let moreThan19Result = firstDifferece * 3
+        console.log(moreThan19Result)
+    } else {
+        console.log(firstDifferece)
+    }
+}
+
+console.log("If the first given numer is more than 19: ")
+crazyDiff(25)
+console.log("If the first given number is less than 19: ")
+crazyDiff(10)
+
 /* EXERCISE 4
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
